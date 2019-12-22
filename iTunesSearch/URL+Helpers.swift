6783c7244ea78e6bd229424163f63668f8869b09
Marc Batete <mybatete@lcmail.lcsc.edu>
2019-12-21 17:14:53 -0800

    iTunes Search App Swift
    iTunes Search App Swift

---
+++ iTunesSearch/URL+Helpers.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Helpers.swift
+//  iTunesSearch
+//
+//  Created by Marc Batete on 12/3/19.
+//  Copyright © 2019 Caleb Hicks. All rights reserved.
+//
+
+import Foundation
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.compactMap { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
+}
